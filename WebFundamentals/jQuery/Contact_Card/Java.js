cc9e37a370d3bb957254f5b0168eaddcdb0490d2
VT-Zhang <jianz@vt.edu>
2017-02-03 16:47:10 -0500

    Added the WeatherApp with the API

--- WebFundamentals/jQuery/Contact_Card/Java.js
+++ WebFundamentals/jQuery/Contact_Card/Java.js
@@ -6,16 +6,21 @@ $(document).ready(function(){
     //     $("#card1").append($("#last_name").val());
     //     $(".description").css("display", "block");
     // });
+    var counter = 1;
 
     $("#add_button").click(function(){
-        $("#right_wrapper").after("<div class="name_card"><p>");
-        $("#right_wrapper").after($("#first_name").val());
-        $("#right_wrapper").after(" ");
-        $("#right_wrapper").after($("#last_name").val());
-        $("#right_wrapper").after("</p><p class="description">Click for Description!</p>");
+        var content = '<div class="name_card" id="' + counter + '"> <p>' + $("#first_name").val() + ' ' + $("#last_name").val() + '</p>';
+        content += '<p class="description">Click for Description!</p></div>';
+        $("#right_wrapper").html(content);
+        counter++;
     });
 
     // $(document).on('click',"text", function(){
     //     $(".name_card").text($("des_box").val());
     // });
+    $(document).on("click", ".name_card", function(){
+        var num = $(this).attr("#id");
+        $('#id' + num).html($("#des_box".val()));
+    });
+
 });

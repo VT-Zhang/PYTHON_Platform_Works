c1eddc12d8eed3edd38735ed6be5392e659cd0f7
VT-Zhang <jianz@vt.edu>
2017-02-06 23:23:08 -0500

    Added a few more Python fundamental practices.

--- Python/Stars.py
+++ Python/Stars.py
@@ -1,17 +1,14 @@
 def star(list):
-    i = 0
     for x in list:
-        print "*" * list[i]
-        i = i + 1
+        print "*" * x
+
 
 star([4,6,1,3,5,7,25])
 
 def star2(list):
-    i = 0
     for x in list:
-        if type(list[i]) is int:
-            print "*" * list[i]
-        elif type(list[i]) is str:
-            print list[i][0].lower() * len(list[i]);
-        i = i + 1
+        if type(x) is int:
+            print "*" * x
+        elif type(x) is str:
+            print x[0].lower() * len(x);
 star2([4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"])

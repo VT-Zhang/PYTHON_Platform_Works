3a3f72e942dadbe3b7b74cace90a42d4fbccb7cf
VT-Zhang <jianz@vt.edu>
2017-02-17 11:49:07 -0500

    Django model practice added.

---
+++ Python/Django/model_practice.py
@@ -0,0 +1,20 @@
+class User(models.Model):
+    first_name = models.Charfield(max_length=255)
+    last_name = models.Charfield(max_length=255)
+    email = models.Charfield(max_length=255)
+    password = models.Charfield(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(autp_now=True)
+
+class Message(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(autp_now=True)
+    user = models.ForeignKey(User)
+
+class Comment(models.Model):
+    comment = message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(autp_now=True)
+    message = models.ForeignKey(Message)
+    user = models.ForeignKey(User)

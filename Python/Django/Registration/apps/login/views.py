5a7f1926e91a134a574e80bf328abe94f2be8ac0
VT-Zhang <jianz@vt.edu>
2017-02-20 22:45:50 -0500

    Finished Django Registration and Login App.

--- Python/Django/Registration/apps/login/views.py
+++ Python/Django/Registration/apps/login/views.py
@@ -18,12 +18,12 @@ def register(request):
         request.session['password'] = request.POST['password']
         request.session['confirm'] = request.POST['confirm']
 
-        request.session['alert'] = '<p>Error(s):</p>'
+        request.session['alert'] = ''
         request.session['alert1'] = 'Registration Success!'
         validation = True
 
         if len(request.session['first_name']) < 2 or len(request.session['last_name']) < 2:
-            request.session['alert'] += '<p>Names cannot be less than 2 letter.</p>'
+            request.session['alert'] += '<p>Error(s):</p><p>Names cannot be less than 2 letter.</p>'
             validation = False
         if str.isalpha(str(request.session['first_name'])) == False or str.isalpha(str(request.session['last_name'])) == False:
             request.session['alert'] += '<p>Names cannot contain any numbers. </p>'
@@ -46,13 +46,15 @@ def register(request):
         if validation == True:
             hashed = bcrypt.hashpw(request.session['password'].encode(encoding="utf-8", errors="strict"), bcrypt.gensalt())
             User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], password=hashed)
+            request.session['name'] = request.POST['first_name']
+            request.session['suc'] = 'You have successfully registered.'
             return render(request, 'login/success.html')
 
 
 def login(request):
     user = User.objects.filter(email=request.POST['log_email'])
-    # if bcrypt.check_password_hash(user[0]['password'], request.POST['log_password']):
-    if bcrypt.hashpw(request.POST['log_password'], user[0]['password']) == user[0]['password']:
+    if bcrypt.checkpw(request.POST['log_password'], user[0]['password']) == True:
+    # if bcrypt.hashpw(request.POST['log_password'], user[0]['password']) == user[0]['password']:
         request.session['suc'] = 'You have successfully logged in.'
         request.session['fail'] = ''
         return render(request, 'login/success.html')
